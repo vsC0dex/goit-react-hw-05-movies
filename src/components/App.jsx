b94e1aa8368b9244d495c1de87b108fc36b14983
vsC0dex <max071010@gmail.com>
2023-02-22 19:17:50 +0200

    add lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,41 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { Triangle } from 'react-loader-spinner';
 
 import Navbar from 'components/Navbar/Navbar';
 
-import HomePage from 'pages/HomePage/HomePage';
-import MoviesPage from 'pages/MoviesPage/MoviesPage';
-import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
-import MovieDetails from '../pages/MovieDetails/MovieDetails';
-import Casts from './Casts/Casts';
-import Reviewers from './Rewiewers/Reviewers';
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('pages/MoviesPage/MoviesPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
+const Casts = lazy(() => import('./Casts/Casts'));
+const Reviewers = lazy(() => import('./Rewiewers/Reviewers'));
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Casts />} />
-          <Route path="reviews" element={<Reviewers />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <Triangle
+            height="180"
+            width="180"
+            color="#4fa94d"
+            ariaLabel="triangle-loading"
+            visible={true}
+          />
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Casts />} />
+            <Route path="reviews" element={<Reviewers />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
